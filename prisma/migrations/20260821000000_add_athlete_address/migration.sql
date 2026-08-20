8d0e5423c93dc26ca4f2933a4550e6774290943f
Douglas Lundy <douglaslundy@gmail.com>
2026-08-20 18:28:06 -0300

    feat: adiciona campos de endereco em AthleteProfile

---
+++ prisma/migrations/20260821000000_add_athlete_address/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "athlete_profiles" ADD COLUMN     "postalCode" TEXT,
+ADD COLUMN     "street" TEXT,
+ADD COLUMN     "number" TEXT,
+ADD COLUMN     "complement" TEXT,
+ADD COLUMN     "neighborhood" TEXT;
